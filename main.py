9f98c27e32d673c0497bbca6e649936f19614687
Elizaveta <krylovaliza@yandex.ru>
2023-01-28 23:01:34 +0300

    Add GAN

--- main.py
+++ main.py
@@ -9,7 +9,17 @@ from aiogram.types.message import ContentType
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters.state import State, StatesGroup
+from model import *
 
+
+model_dict = torch.load('pretrained.model')
+model_dict_clone = model_dict.copy()
+for key, value in model_dict_clone.items():
+    if key.endswith(('running_mean', 'running_var')):
+        del model_dict[key]
+
+style_model = Net(ngf=128)
+style_model.load_state_dict(model_dict, False)
 
 token = pathlib.Path('token.txt').read_text()
 
@@ -18,22 +28,28 @@ bot = Bot(token=token)
 storage = MemoryStorage()
 dp = Dispatcher(bot, storage=storage)
 cur_path = os.getcwd()
-
 # общие команды
 
 
 @dp.message_handler(commands=['start'])
 async def cmd_start(message: types.Message):
-    await message.answer("Приветствую Вас в боте переноса стиля! \n\nСправку по командам можно посмотреть так /help")
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    buttons = ["Загрузить фото для обработки", "Отменить операцию", "Помощь"]
+    for b in buttons:
+        keyboard.add(b)
+
+    await message.answer("Приветствую Вас в боте переноса стиля! \n\nСправку по командам можно посмотреть так /help", reply_markup=keyboard)
 
 
+@dp.message_handler(lambda message: message.text == "Помощь")
 @dp.message_handler(commands=['help'])
 async def cmd_help(message: types.Message):
     msg = text(bold('Я могу ответить на следующие команды:'),
-               '/photo - загрузка фотографии и желаемого стиля', '/cancel - отмена операции', '/help - справка по работе бота', sep='\n')
+               '/transfer - загрузка фотографии и желаемого стиля', '/cancel - отмена операции', '/help - справка по работе бота', sep='\n')
     await message.answer(msg, parse_mode=ParseMode.MARKDOWN)
 
 
+@dp.message_handler(lambda message: message.text == "Отменить операцию")
 @dp.message_handler(commands=['cancel'], state='*')
 async def cmd_cancel(message: types.Message, state: FSMContext):
     await state.finish()
@@ -47,7 +63,8 @@ class GetImage(StatesGroup):
     style = State()
 
 
-@dp.message_handler(commands=['photo'], state='*')
+@dp.message_handler(lambda message: message.text == "Загрузить фото для обработки")
+@dp.message_handler(commands=['transfer'], state='*')
 async def cmd_photo(message: types.Message, state: FSMContext):
     await message.answer('Загрузите фото для изменения!')
     await state.set_state(GetImage.image.state)
@@ -65,15 +82,20 @@ async def process_photo(message: types.Message, state: FSMContext):
 async def process_photo(message: types.Message, state: FSMContext):
     destination_file = cur_path + "\photos\style.jpg"
     await message.photo[-1].download(destination_file=destination_file)
-    await message.answer('Все фото загружены!')
-    await bot.send_photo(chat_id=message.from_user.id, photo=message.photo[-1].file_id)
+    await message.answer('Все фото загружены! \nМне потребуется немного времени для обработки, подождите, пожалуйста!')
     await state.finish()
+
+    image_size = 400
+    transform(style_model, cur_path + "\photos\content.jpg",
+              cur_path + "\photos\style.jpg", image_size, filename=cur_path +'\photos\\result.jpg')
+    with open(cur_path +'\photos\\result.jpg', 'rb') as file:
+        await message.answer_photo(file, caption='Готово!')
 
 
 @dp.message_handler(content_types=ContentType.ANY)
 async def unknown_message(msg: types.Message):
     message_text = text(
-        'Я не знаю, что с этим делать \n Посмотрите доступные действия по команде /help')
+        'Я не знаю, что с этим делать \nПосмотрите доступные действия по команде /help')
     await msg.reply(message_text, parse_mode=ParseMode.MARKDOWN)
 
 
